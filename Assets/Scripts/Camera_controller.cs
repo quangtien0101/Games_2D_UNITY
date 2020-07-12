14019efe264a2caaa0f4e0015cbb9aeaa8b0875e
quangtien0101 <elwrci.quangtien@gmail.com>
2020-07-12 12:20:55 +0700

    HW1
    Character controller for main character
    Animation for walking/idle for mobs and character
    Implement bounding box so player can't walk into water...

---
+++ Assets/Scripts/Camera_controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_controller : MonoBehaviour
+{
+    public GameObject follow_target;
+    private Vector3 target_position;
+    public float move_speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target_position = new Vector3(follow_target.transform.position.x, follow_target.transform.position.y, transform.position.z);
+
+        // make the camera move to new position
+        transform.position = Vector3.Lerp(transform.position, target_position, move_speed * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    public float move_Speed;
+    
+    private SpriteRenderer player_sprite;
+    private Animator anim;
+    private Rigidbody2D player_rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Awake()
+    {
+        player_sprite = GetComponent<SpriteRenderer>();
+        player_rigid = GetComponent<Rigidbody2D>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x_axis_input = Input.GetAxisRaw("Horizontal");
+        float y_axis_input = Input.GetAxisRaw("Vertical");
+
+        // control the facing of the characters
+
+        if (x_axis_input != 0)
+        {
+            if (x_axis_input > 0.5f && (player_sprite.flipX == true)) // if the direction is to the right and is facing to the left
+            {
+                player_sprite.flipX = false;    //flip back
+            }
+            else if (x_axis_input < 0.5f && (player_sprite.flipX == false))
+            {
+
+                player_sprite.flipX = true;
+            }
+        }
+        float move_horizontal = x_axis_input * move_Speed; //* Time.deltaTime;
+        float move_vertical = y_axis_input * move_Speed; //* Time.deltaTime; 
+
+        //transform.Translate(new Vector3(move_horizontal, move_vertical, 0f));
+        
+
+        player_rigid.velocity = new Vector2(move_horizontal, move_vertical); 
+
+
+        anim.SetFloat("MoveX", x_axis_input);
+        anim.SetFloat("MoveY", y_axis_input);
+    }
+}
